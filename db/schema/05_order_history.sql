50e5710e57593158842bacc9a31113218db41149
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-06-04 16:05:09 -0400

    add the products cart and order tables

---
+++ db/schema/05_order_history.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS order_history CASCADE;
+CREATE TABLE order_history (
+  id SERIAL PRIMARY KEY NOT NULL,
+  cart_id INTEGER REFERENCES cart(id) ON DELETE CASCADE,
+  buyer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  order_date date
+);
