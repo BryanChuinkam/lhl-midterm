cbe179adb649b50a4026b0a8b02189cbfc057946
= <=>
2023-06-05 11:59:18 -0400

    updated html files for ejs files

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,29 @@ const router  = express.Router();
 
 router.get('/', (req, res) => {
   res.render('users');
-}); 
+});
+
+module.exports = router;
+
+
+// ROUTING FOR REGISTER
+// ----------------------------
+// Handle GET request for the registration page
+router.get('/register', (req, res) => {
+  res.sendFile('lhl-midterm/views/register.html'); // Replace with the actual path to your HTML file
+});
+
+// Handle POST request for the registration form submission
+router.post('/register', (req, res) => {
+  const { email, password, city, province, phone } = req.body;
+  console.log('Submitted form data:');
+  console.log('Email:', email);
+  console.log('Password:', password);
+  console.log('City:', city);
+  console.log('Province:', province);
+  console.log('Phone Number:', phone);
+
+  res.redirect('/home');
+});
 
 module.exports = router;
