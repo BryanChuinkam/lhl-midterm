2152c3ba9f4b978f8558d42983c957f4bc31815b
= <=>
2023-06-07 10:11:32 -0400

    continued working on register functionality

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,6 @@
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into /users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
 const express = require('express');
 const router = express.Router();
+const db = require('../db/queries/users');
 
 // Handle GET request for the registration page
 router.get('/register', (req, res) => {
@@ -14,16 +9,32 @@ router.get('/register', (req, res) => {
 
 // Handle POST request for the registration form submission
 router.post('/register', (req, res) => {
-  const { email, password, city, province, phone } = req.body;
+  console.log('Reached the /users/register route handler');
+  const { user_name, email, password, city, province, phone_number } = req.body;
   console.log('Submitted form data:');
+  console.log('Name:', user_name);
   console.log('Email:', email);
   console.log('Password:', password);
   console.log('City:', city);
   console.log('Province:', province);
-  console.log('Phone Number:', phone);
+  console.log('Phone Number:', phone_number);
 
-  // Redirect or render a success page if needed
+  db.getUserByEmailOrPhoneNumber(email, phone_number)
+    .then((existingUser) => {
+      if (existingUser) {
+        const errorMessage = 'Invalid credentials - This information has already been used.';
+        res.render('register', { errorMessage });
+      } else {
+        return db.createUser(user_name, email, password, city, province, phone_number)
+          .then(() => {
+            console.log('User created successfully');
+            res.redirect('/');
+          });
+      }
+    })
+    .catch((err) => {
+      console.error('Error creating user:', err);
+      res.redirect('/users/register');
+    });
 });
-
 module.exports = router;
-
